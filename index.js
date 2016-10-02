9419412dc02d81ea4f1e5e7a957fd405de58d488
Hamdan Javeed <hamdan@hamdanjaveed.com>
2016-10-02 16:13:19 -0400

    Add animation, fix up rotation

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ var controls;
 
 var raycaster, mouse;
 
+var rotating = false;
+
 var SIZE = 100;
 var MARGIN = 5;
 
@@ -30,12 +32,12 @@ var BLUE = 0x2196f3;
 var CUBE_COLORS = [RED, ORANGE, WHITE, YELLOW, GREEN, BLUE];
 // TODO: replace with mouse drag events
 var keyToTurnMap = {
-  49: function(shiftKeyDown) { doF(shiftKeyDown) }, // 1
-  50: function(shiftKeyDown) { doB(shiftKeyDown) }, // 2
-  51: function(shiftKeyDown) { doL(shiftKeyDown) }, // 3
-  52: function(shiftKeyDown) { doR(shiftKeyDown) }, // 4
-  53: function(shiftKeyDown) { doU(shiftKeyDown) }, // 5
-  54: function(shiftKeyDown) { doD(shiftKeyDown) }, // 6
+    49: function(shiftKeyDown) { doF(shiftKeyDown) }, // 1
+    50: function(shiftKeyDown) { doB(shiftKeyDown) }, // 2
+    51: function(shiftKeyDown) { doL(shiftKeyDown) }, // 3
+    52: function(shiftKeyDown) { doR(shiftKeyDown) }, // 4
+    53: function(shiftKeyDown) { doU(shiftKeyDown) }, // 5
+    54: function(shiftKeyDown) { doD(shiftKeyDown) }, // 6
 }
 
 var X_AXIS;
@@ -45,183 +47,255 @@ var Z_AXIS;
 init();
 
 function init() {
-  // waits for three.js to load
-  setTimeout(function() {
-    X_AXIS = new THREE.Vector3(1,0,0);
-    Y_AXIS = new THREE.Vector3(0,1,0);
-    Z_AXIS = new THREE.Vector3(0,0,1);
+    // waits for three.js to load
+    setTimeout(function() {
+        X_AXIS = new THREE.Vector3(1, 0, 0);
+        Y_AXIS = new THREE.Vector3(0, 1, 0);
+        Z_AXIS = new THREE.Vector3(0, 0, 1);
 
-    raycaster = new THREE.Raycaster();
-    mouse = new THREE.Vector2();
-    scene = new THREE.Scene();
-    pivot = new THREE.Object3D();
+        raycaster = new THREE.Raycaster();
+        mouse = new THREE.Vector2();
+        scene = new THREE.Scene();
+        pivot = new THREE.Object3D();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+        camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+        camera.position.z = 1000;
 
-    // init camera controls
-    controls = new THREE.TrackballControls(camera);
-    controls.rotateSpeed = 5.0;
-		controls.zoomSpeed = 1.0;
-		controls.noZoom = false;
-		controls.noPan = true;
-		controls.staticMoving = true;
-		controls.dynamicDampingFactor = 0.3;
-		controls.keys = [ 65, 83, 68 ];
-		controls.addEventListener( 'change', render );
+        // init camera controls
+        controls = new THREE.TrackballControls(camera);
+        controls.rotateSpeed = 5.0;
+        controls.zoomSpeed = 1.0;
+        controls.noZoom = false;
+        controls.noPan = true;
+        controls.staticMoving = true;
+        controls.dynamicDampingFactor = 0.3;
+        controls.keys = [65, 83, 68];
+        controls.addEventListener('change', render);
 
-    // init cube piece geometry and colors
-    geometry = new THREE.BoxGeometry(SIZE, SIZE, SIZE);
-    for ( var i = 0; i < geometry.faces.length; i+=2  ) {
-      geometry.faces[ i ].color.setHex(CUBE_COLORS[i/2]);
-      geometry.faces[ i+1 ].color.setHex(CUBE_COLORS[i/2]);
-    }
-    material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } );
+        // init cube piece geometry and colors
+        geometry = new THREE.BoxGeometry(SIZE, SIZE, SIZE);
+        for (var i = 0; i < geometry.faces.length; i += 2) {
+            geometry.faces[i].color.setHex(CUBE_COLORS[i / 2]);
+            geometry.faces[i + 1].color.setHex(CUBE_COLORS[i / 2]);
+        }
+        material = new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+            vertexColors: THREE.FaceColors
+        });
 
-    // create cube pieces
-    for (var i = 0; i < 27; i++) {
-      cubeMeshArray[i] = new THREE.Mesh( geometry, material );
-      cubeMeshArray[i].position.x = (i * (SIZE+MARGIN)) % ((SIZE+MARGIN)*3) - (SIZE+MARGIN);
-      cubeMeshArray[i].position.y = (Math.floor((i%9)/3) * (SIZE+MARGIN)) - (SIZE+MARGIN);
-      cubeMeshArray[i].position.z = Math.floor(i/9) * (SIZE+MARGIN) - (SIZE+MARGIN);
-      cubeMeshArray[i].callback = function() { console.log('click piece'); }
-      scene.add( cubeMeshArray[i] );
-    }
-    scene.add(pivot);
+        // create cube pieces
+        for (var i = 0; i < 27; i++) {
+            cubeMeshArray[i] = new THREE.Mesh(geometry, material);
+            cubeMeshArray[i].position.x = (i * (SIZE + MARGIN)) % ((SIZE + MARGIN) * 3) - (SIZE + MARGIN);
+            cubeMeshArray[i].position.y = (Math.floor((i % 9) / 3) * (SIZE + MARGIN)) - (SIZE + MARGIN);
+            cubeMeshArray[i].position.z = Math.floor(i / 9) * (SIZE + MARGIN) - (SIZE + MARGIN);
+            cubeMeshArray[i].callback = function() {
+                console.log('click piece');
+            }
+            scene.add(cubeMeshArray[i]);
+        }
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+        scene.add(pivot);
 
-    // init test event listeners
-    document.addEventListener('keydown', function(e) {
-      var action = keyToTurnMap[e.keyCode];
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
 
-      if (action) {
-        action(e.shiftKey);
-      }
-    });
-    document.addEventListener('mousedown', function(e) {
-      onDocumentMouseDown(e);
-    });
+        // init test event listeners
+        document.addEventListener('keydown', function(e) {
+            var action = keyToTurnMap[e.keyCode];
 
-    document.body.appendChild( renderer.domElement );
-    animate();
-    render();
-  }, 500);
+            if (action) {
+                action(e.shiftKey);
+            }
+        });
+        document.addEventListener('mousedown', function(e) {
+            onDocumentMouseDown(e);
+        });
+
+        document.body.appendChild(renderer.domElement);
+        animate();
+        render();
+    }, 500);
 }
 
 function animate() {
-  requestAnimationFrame( animate );
-  controls.update();
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
 }
 
 function render() {
-  renderer.render(scene, camera);
+    TWEEN.update();
+    renderer.render(scene, camera);
 }
 
 // source: http://stackoverflow.com/questions/12800150/catch-the-click-event-on-a-specific-mesh-in-the-renderer
 function onDocumentMouseDown(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  mouse.x = ( e.clientX / renderer.domElement.clientWidth ) * 2 - 1;
-  mouse.y = - ( e.clientY / renderer.domElement.clientHeight ) * 2 + 1;
+    mouse.x = (e.clientX / renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = -(e.clientY / renderer.domElement.clientHeight) * 2 + 1;
 
-  raycaster.setFromCamera( mouse, camera );
+    raycaster.setFromCamera(mouse, camera);
 
-  var intersects = raycaster.intersectObjects(cubeMeshArray);
+    var intersects = raycaster.intersectObjects(cubeMeshArray);
 
-  if (intersects.length > 0) {
-      // intersects[0].object.callback();
-      // console.log(intersects[0].object);
-  }
+    if (intersects.length > 0) {
+        // intersects[0].object.callback();
+        // console.log(intersects[0].object);
+    }
+}
+
+// ***---*** ROTATION ***---*** //
+
+function getGroup(toRotate) {
+    var group = new THREE.Object3D();
+    for (var i = 0; i < toRotate.length; i++) {
+        scene.remove(toRotate[i]);
+        group.add(toRotate[i]);
+    }
+    scene.add(group);
+    return group;
+}
+
+function rotateCallback(toRotate, group) {
+    scene.updateMatrixWorld();
+    for (var i = 0; i < toRotate.length; i++) {
+        toRotate[i].applyMatrix(group.matrixWorld);
+        group.remove(toRotate[i]);
+        scene.add(toRotate[i]);
+    }
+
+    rotating = false;
+}
+
+function getRotation(doPrime) {
+    return doPrime ? -Math.PI / 2 : Math.PI / 2;
+}
+
+function getCubes(face) {
+    switch (face) {
+    case 0:
+        return cubeMeshArray.filter(function(obj, i) {
+            return SIZE <= obj.position.z && obj.position.z < SIZE + 2 * MARGIN;
+        });
+    case 1:
+        return cubeMeshArray.filter(function(obj, i) {
+            return -(SIZE + 2 * MARGIN) <= obj.position.z && obj.position.z < -SIZE;
+        });
+    case 2:
+        return cubeMeshArray.filter(function(obj, i) {
+            return -(SIZE + 2 * MARGIN) <= obj.position.x && obj.position.x < -SIZE;
+        });
+    case 3:
+        return cubeMeshArray.filter(function(obj, i) {
+            return SIZE <= obj.position.x && obj.position.x < SIZE + 2 * MARGIN;
+        });
+    case 4:
+        return cubeMeshArray.filter(function(obj, i) {
+            return SIZE <= obj.position.x && obj.position.x < SIZE + 2 * MARGIN;
+        });
+    case 5:
+        return cubeMeshArray.filter(function(obj, i) {
+            return SIZE <= obj.position.y && obj.position.y < SIZE + 2 * MARGIN;
+        });
+    case 6:
+        return cubeMeshArray.filter(function(obj, i) {
+            return -(SIZE + 2 * MARGIN) <= obj.position.y && obj.position.y < -SIZE;
+        });
+    }
 }
 
 // do* functions perform cube instructions
 // @param doPrime  performs instruction CCW if true
 function doF(doPrime = false) {
-  var toRotate = cubeMeshArray.filter(function(obj, i) {
-    return SIZE <= obj.position.z && obj.position.z < SIZE + 2*MARGIN;
-  });
+    if (rotating) return;
+    rotating = true;
 
-  startRotation(toRotate);
-  rotateAroundWorldAxis(pivot, Z_AXIS, doPrime ? Math.PI / 2 : Math.PI * 3/2);
-  render();
-  endRotation(toRotate);
+    var toRotate = getCubes(0);
+    var group = getGroup(toRotate);
+    rotateAroundWorldAxis(group, Z_AXIS, getRotation(doPrime), function() {
+        rotateCallback(toRotate, group);
+    });
 }
+
 function doB(doPrime = false) {
-  var toRotate = cubeMeshArray.filter(function(obj, i) {
-    return -(SIZE + 2*MARGIN) <= obj.position.z && obj.position.z <  -SIZE;
-  });
+    if (rotating) return;
+    rotating = true;
 
-  startRotation(toRotate);
-  rotateAroundWorldAxis(pivot, Z_AXIS, doPrime ? Math.PI * 3/2 : Math.PI / 2);
-  render();
-  endRotation(toRotate);
+    var toRotate = getCubes(1);
+    var group = getGroup(toRotate);
+    rotateAroundWorldAxis(group, Z_AXIS, getRotation(doPrime), function() {
+        rotateCallback(toRotate, group);
+    });
 }
+
 function doL(doPrime = false) {
-  var toRotate = cubeMeshArray.filter(function(obj, i) {
-    return -(SIZE + 2*MARGIN) <= obj.position.x && obj.position.x <  -SIZE;
-  });
+    if (rotating) return;
+    rotating = true;
 
-  startRotation(toRotate);
-  rotateAroundWorldAxis(pivot, X_AXIS, doPrime ? Math.PI * 3/2  : Math.PI / 2);
-  render();
-  endRotation(toRotate);
+    var toRotate = getCubes(2);
+    var group = getGroup(toRotate);
+    rotateAroundWorldAxis(group, X_AXIS, getRotation(doPrime), function() {
+        rotateCallback(toRotate, group);
+    });
 }
+
 function doR(doPrime = false) {
-  var toRotate = cubeMeshArray.filter(function(obj, i) {
-    return SIZE <= obj.position.x && obj.position.x < SIZE + 2*MARGIN;
-  });
+    if (rotating) return;
+    rotating = true;
 
-  startRotation(toRotate);
-  rotateAroundWorldAxis(pivot, X_AXIS, doPrime ? Math.PI / 2  : Math.PI * 3/2);
-  render();
-  endRotation(toRotate);
+    var toRotate = getCubes(3);
+    var group = getGroup(toRotate);
+    rotateAroundWorldAxis(group, X_AXIS, getRotation(doPrime), function() {
+        rotateCallback(toRotate, group);
+    });
 }
+
 function doU(doPrime = false) {
-  var toRotate = cubeMeshArray.filter(function(obj, i) {
-    return SIZE <= obj.position.y && obj.position.y < SIZE + 2*MARGIN;
-  });
+    if (rotating) return;
+    rotating = true;
 
-  startRotation(toRotate);
-  rotateAroundWorldAxis(pivot, Y_AXIS, doPrime ? Math.PI / 2 : Math.PI * 3/2);
-  render();
-  endRotation(toRotate);
+    var toRotate = getCubes(5);
+    var group = getGroup(toRotate);
+    rotateAroundWorldAxis(group, Y_AXIS, getRotation(doPrime), function() {
+        rotateCallback(toRotate, group);
+    });
 }
+
 function doD(doPrime = false) {
-  var toRotate = cubeMeshArray.filter(function(obj, i) {
-    return -(SIZE + 2*MARGIN) <= obj.position.y && obj.position.y <  -SIZE;
-  });
+    if (rotating) return;
+    rotating = true;
 
-  startRotation(toRotate);
-  rotateAroundWorldAxis(pivot, Y_AXIS, doPrime ? Math.PI * 3/2 : Math.PI / 2);
-  render();
-  endRotation(toRotate);
+    var toRotate = getCubes(6);
+    var group = getGroup(toRotate);
+    rotateAroundWorldAxis(group, Y_AXIS, getRotation(doPrime), function() {
+        rotateCallback(toRotate, group);
+    });
 }
 
-function startRotation(toRotate) {
-  pivot.rotation.set(0,0,0);
-  pivot.updateMatrixWorld();
-  for (i in toRotate) {
-    THREE.SceneUtils.attach(toRotate[i], scene, pivot);
-  }
-}
-function endRotation(rotated) {
-  pivot.updateMatrixWorld();
-  for (i in rotated) {
-    rotated[i].updateMatrixWorld();
-    THREE.SceneUtils.detach(rotated[i], pivot, scene);
-  }
-}
-
-// This function rotates mesh objects about the world axis
-// source: http://stackoverflow.com/questions/11060734/how-to-rotate-a-3d-object-on-axis-three-js
-var rotWorldMatrix;
 // Rotate an object around an arbitrary axis in world space
-function rotateAroundWorldAxis(object, axis, radians) {
-    rotWorldMatrix = new THREE.Matrix4();
-    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
-    rotWorldMatrix.multiply(object.matrix); // pre-multiply
-    object.matrix = rotWorldMatrix;
-    object.rotation.setFromRotationMatrix(object.matrix);
+function rotateAroundWorldAxis(object, axis, radians, callback) {
+    var rot;
+    if (axis === Z_AXIS) {
+        rot = { z: radians }
+    } else if (axis === X_AXIS) {
+        rot = { x: radians }
+    } else {
+        rot = { y: radians }
+    }
+
+    var t = new TWEEN.Tween(object.rotation)
+        .to(rot, 50)
+        .onUpdate(function() {
+            if (axis === Z_AXIS) {
+                object.rotation.z = this.z;
+            } else if (axis === X_AXIS) {
+                object.rotation.x = this.x;
+            } else {
+                object.rotation.y = this.y;
+            }
+        })
+        .onComplete(callback)
+        .start();
 }
